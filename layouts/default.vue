2afea7638adc11e12e5489de2389b9eb4bc89d24
renalfarid <enal.farid@gmail.com>
2024-06-27 13:18:52 +0800

    remove page insight

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,10 @@
 <script setup>
-  import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
 <template>
-    <SpeedInsights>
-     <TopMenu />
-     <Announcement />
-     <div class="mx-auto max-w-screen-xl py-6 px-4 sm:px-6 lg:px-8">
+    <TopMenu />
+    <Announcement />
+    <div class="mx-auto max-w-screen-xl py-6 px-4 sm:px-6 lg:px-8">
         <slot />
-     </div>
-     <UseFooter />
-    </SpeedInsights>
+    </div>
+    <UseFooter />
 </template>
