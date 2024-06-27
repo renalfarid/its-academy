8f1f3ef8295baf2735325f11289392ce624809d4
renalfarid <enal.farid@gmail.com>
2024-06-27 13:13:33 +0800

    speed insight

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
+<script setup>
+  import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
 <template>
-    <TopMenu />
-    <Announcement />
-    <div class="mx-auto max-w-screen-xl py-6 px-4 sm:px-6 lg:px-8">
+    <SpeedInsights>
+     <TopMenu />
+     <Announcement />
+     <div class="mx-auto max-w-screen-xl py-6 px-4 sm:px-6 lg:px-8">
         <slot />
-    </div>
-    <UseFooter />
+     </div>
+     <UseFooter />
+    </SpeedInsights>
 </template>
