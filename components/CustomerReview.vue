d01a5138dd7439086cd8b30c8d93602da27bdcc7
renalfarid <enal.farid@gmail.com>
2024-06-28 15:02:15 +0800

    fix review

--- components/CustomerReview.vue
+++ components/CustomerReview.vue
@@ -10,7 +10,7 @@
             <div class="flex items-center gap-4">
               <img
                 alt=""
-                src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                src="/img/pelindo.jpeg"
                 class="size-14 rounded-full object-cover"
               />
     
@@ -68,14 +68,12 @@
                   </svg>
                 </div>
     
-                <p class="mt-0.5 text-lg font-medium text-gray-900">Paul Starr</p>
+                <p class="mt-0.5 text-lg font-medium text-gray-900">Nusair</p>
               </div>
             </div>
     
             <p class="mt-4 text-gray-700">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa sit rerum incidunt, a
-              consequuntur recusandae ab saepe illo est quia obcaecati neque quibusdam eius accusamus
-              error officiis atque voluptates magnam!
+                Kami telah bekerja sama dengan ITS sejak tahun 2011, mulai dari pekerjaan Software Development, CCTV, Palang parkir, raised floor, jaringan LAN, fiber optik dan kebutuhan Teknologi Informasi usaha kami, didukung dengan pengalaman  dan sumber dayamenjadi parnert terpercaya - Pelindo.
             </p>
           </blockquote>
     
@@ -83,7 +81,7 @@
             <div class="flex items-center gap-4">
               <img
                 alt=""
-                src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                src="/img/marlip.jpeg"
                 class="size-14 rounded-full object-cover"
               />
     
@@ -141,14 +139,12 @@
                   </svg>
                 </div>
     
-                <p class="mt-0.5 text-lg font-medium text-gray-900">Paul Starr</p>
+                <p class="mt-0.5 text-lg font-medium text-gray-900">Masrah Marang</p>
               </div>
             </div>
     
             <p class="mt-4 text-gray-700">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa sit rerum incidunt, a
-              consequuntur recusandae ab saepe illo est quia obcaecati neque quibusdam eius accusamus
-              error officiis atque voluptates magnam!
+                Saya menyambut gembira atas pencapaian perkembangan usaha ITS, kami telah bekerja sama dalam beberapa project di Makassar dan Bandung dalam memenuhi kebutuhan Teknologi Informasi usaha kami.  Sukses terus berkembang menjadi yang terbaik. - PT Marlip Indonesia
             </p>
           </blockquote>
     
@@ -156,7 +152,7 @@
             <div class="flex items-center gap-4">
               <img
                 alt=""
-                src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                src="/img/tonasa.png"
                 class="size-14 rounded-full object-cover"
               />
     
@@ -214,14 +210,12 @@
                   </svg>
                 </div>
     
-                <p class="mt-0.5 text-lg font-medium text-gray-900">Paul Starr</p>
+                <p class="mt-0.5 text-lg font-medium text-gray-900">Hakmal Candra</p>
               </div>
             </div>
     
             <p class="mt-4 text-gray-700">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa sit rerum incidunt, a
-              consequuntur recusandae ab saepe illo est quia obcaecati neque quibusdam eius accusamus
-              error officiis atque voluptates magnam!
+                Kami telah bekerja sama dengan ITS untuk memenuhi kebutuhan Palang parkir kami dibeberapa cabang PT Semen Tonasa dibawah Group PT Semen Indonesia, lokasi pemasangan di Balikpapan, Samarinda, Pontianak dan Banjarmasin. Hasilnya sangat memuaskan, dan Profesional. Sukses selalu ITS - PT Semen Tonasa
             </p>
           </blockquote>
         </div>
