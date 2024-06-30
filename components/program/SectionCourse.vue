3c18d9b647ec40bd8c75765fe7b75a58a58e74cb
renalfarid <enal.farid@gmail.com>
2024-06-30 19:14:59 +0800

    fix training page

--- components/program/SectionCourse.vue
+++ components/program/SectionCourse.vue
@@ -1,3 +1,39 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const kelas = ref([
+    {
+      'nama': 'Kelas Backend',
+      'deskripsi': 'Pelatihan ini mencakup pemrograman dengan bahasa seperti Golang, PHP, atau Node.js serta memahami basis data, autentikasi, dan pengelolaan API. Dapatkan keahlian untuk menciptakan aplikasi web yang kuat dan scalable.',
+      'icon': 'laptop-code'
+    },
+    {
+      'nama': 'Kelas Frontend',
+      'deskripsi': 'Pelatihan ini mencakup HTML, CSS, dan JavaScript, serta framework populer seperti Vue atau Nuxt. Anda akan belajar bagaimana membuat antarmuka pengguna yang responsif dan interaktif.',
+      'icon': 'window-restore'
+    },
+    {
+      'nama': 'Kelas Devops',
+      'deskripsi': 'Pelatihan ini mengajarkan bagaimana mengintegrasikan dan mengotomatiskan proses pengembangan dan operasi. Anda akan belajar tentang Continuous Integration, Continuous Deployment, serta tools seperti Docker, Kubernetes.',
+      'icon': 'code-branch'
+    },
+    {
+      'nama': 'Kelas Fullstack',
+      'deskripsi': 'Menjadi developer serba bisa dengan memahami kedua sisi pengembangan web, baik frontend maupun backend. Pelatihan ini mencakup pengetahuan menyeluruh dari HTML, CSS, JavaScript, hingga basis data dan server-side programming, memungkinkan Anda untuk membangun aplikasi web lengkap dari awal hingga akhir.',
+      'icon': 'cubes'
+    },
+    {
+      'nama': 'Kelas Network',
+      'deskripsi': 'Mempelajari dasar-dasar jaringan komputer, konfigurasi perangkat jaringan, dan keamanan jaringan. Pelatihan ini mencakup pemahaman tentang protokol jaringan, perancangan topologi jaringan, serta troubleshooting jaringan untuk memastikan kelancaran komunikasi data.',
+      'icon': 'network-wired'
+    },
+    {
+      'nama': 'Kelas IoT',
+      'deskripsi': 'Memahami teknologi Internet of Things (IoT) dan bagaimana menghubungkan perangkat ke internet. Pelatihan ini mencakup pemrograman sensor dan aktuator, pengumpulan dan analisis data, serta pengelolaan perangkat IoT.',
+      'icon': 'microchip'
+    },
+  ])
+</script>
 <template>
     <section class="bg-white">
       <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
@@ -11,94 +47,22 @@
     
         <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           <a
+            v-for="item in kelas" 
+            :key="item.nama"
             class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
             href="#"
           >
             <span style="font-size: 1em; color: Magenta;">
-                <font-awesome-icon icon="laptop-code" />
+                <font-awesome-icon :icon="item.icon" />
             </span>
     
-            <h2 class="mt-4 text-xl font-bold">Training Backend</h2>
+            <h2 class="mt-4 text-xl font-bold">{{ item.nama }}</h2>
     
             <p class="mt-1 text-sm text-gray-800">
-                Pelatihan ini mencakup pemrograman dengan bahasa seperti Golang, PHP, atau Node.js serta memahami basis data, autentikasi, dan pengelolaan API. Dapatkan keahlian untuk menciptakan aplikasi web yang kuat dan scalable.
+                {{ item.deskripsi }}
             </p>
           </a>
     
-          <a
-            class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-            href="#"
-          >
-          <span style="font-size: 1em; color: Magenta;">
-            <font-awesome-icon icon="window-restore" />
-          </span>
-    
-            <h2 class="mt-4 text-xl font-bold">Training Frontend</h2>
-    
-            <p class="mt-1 text-sm text-gray-800">
-                Pelatihan ini mencakup HTML, CSS, dan JavaScript, serta framework populer seperti Vue atau Nuxt. Anda akan belajar bagaimana membuat antarmuka pengguna yang responsif dan interaktif.
-            </p>
-          </a>
-    
-          <a
-            class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-            href="#"
-          >
-            <span style="font-size: 1em; color: Magenta;">
-                <font-awesome-icon icon="code-branch" />
-            </span>
-    
-            <h2 class="mt-4 text-xl font-bold">Training Devops</h2>
-    
-            <p class="mt-1 text-sm text-gray-800">
-                Pelatihan ini mengajarkan bagaimana mengintegrasikan dan mengotomatiskan proses pengembangan dan operasi. Anda akan belajar tentang Continuous Integration, Continuous Deployment, serta tools seperti Docker, Kubernetes.
-            </p>
-          </a>
-    
-          <a
-            class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-            href="#"
-          >
-            <span style="font-size: 1em; color: Magenta;">
-                <font-awesome-icon icon="cubes" />
-            </span>
-    
-            <h2 class="mt-4 text-xl font-bold">Training Fullstack</h2>
-    
-            <p class="mt-1 text-sm text-gray-800">
-                Menjadi developer serba bisa dengan memahami kedua sisi pengembangan web, baik frontend maupun backend. Pelatihan ini mencakup pengetahuan menyeluruh dari HTML, CSS, JavaScript, hingga basis data dan server-side programming, memungkinkan Anda untuk membangun aplikasi web lengkap dari awal hingga akhir.
-            </p>
-          </a>
-    
-          <a
-            class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-            href="#"
-          >
-            <span style="font-size: 1em; color: Magenta;">
-                <font-awesome-icon icon="network-wired" />
-            </span>
-    
-            <h2 class="mt-4 text-xl font-bold">Training Network</h2>
-    
-            <p class="mt-1 text-sm text-gray-800">
-                Mempelajari dasar-dasar jaringan komputer, konfigurasi perangkat jaringan, dan keamanan jaringan. Pelatihan ini mencakup pemahaman tentang protokol jaringan, perancangan topologi jaringan, serta troubleshooting jaringan untuk memastikan kelancaran komunikasi data.
-            </p>
-          </a>
-    
-          <a
-            class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-            href="#"
-          >
-            <span style="font-size: 1em; color: Magenta;">
-                <font-awesome-icon icon="microchip" />
-            </span>
-    
-            <h2 class="mt-4 text-xl font-bold">Training IoT</h2>
-    
-            <p class="mt-1 text-sm text-gray-800">
-                Memahami teknologi Internet of Things (IoT) dan bagaimana menghubungkan perangkat ke internet. Pelatihan ini mencakup pemrograman sensor dan aktuator, pengumpulan dan analisis data, serta pengelolaan perangkat IoT.
-            </p>
-          </a>
         </div>
     
         <div class="mt-12 text-center">
