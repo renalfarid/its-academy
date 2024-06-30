9c27431741ca37a1c1cfa9bb9d39ececceca608c
renalfarid <enal.farid@gmail.com>
2024-06-30 14:35:21 +0800

    fix env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,9 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      SPREAD_SHEET_ID: process.env.SPREAD_SHEET_ID,
-      GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
-      GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY
+      SPREAD_SHEET_ID: process.env.NUXT_PUBLIC_SPREAD_SHEET_ID,
+      GOOGLE_CLIENT_EMAIL: process.env.NUXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+      GOOGLE_PRIVATE_KEY: process.env.NUXT_PUBLIC_GOOGLE_PRIVATE_KEY
     }
   },
   devtools: { enabled: false },
