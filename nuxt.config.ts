65238137a833295ccb7d553fd5729df2b4c2bcf9
renalfarid <enal.farid@gmail.com>
2024-06-30 18:56:52 +0800

    fix seo title page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: "icon", type: "image/png", href: "/img/its-logo.png" }
+        { rel: "icon", type: "image/png", href: "/its-logo.png" }
       ]
     }
   },
