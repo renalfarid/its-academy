b889d2babdfaf80791d9642f2cbf2153027ca4d7
renalfarid <enal.farid@gmail.com>
2024-07-01 14:10:23 +0800

    optimize speed performance

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ useHead({
     { property: 'og:title', content: `Raih Karir Impian bersama ITS Academy` },
     { property: 'og:description', content: 'ITS Academy hadir untuk membantu Anda mengasah keterampilan IT melalui program-program pelatihan yang komprehensif dan praktis.' },
     { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: 'https://its-academy.com' },
+    { property: 'og:url', content: 'https://its-academy.vercel.app' },
     { property: 'og:locale', content: 'id_ID' },
     { property: 'og:image', content: 'https://its-academy.vercel.app/img/computer-class.png' },
   ]
