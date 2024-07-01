4a6849a77aa30977aab09b2f799021f1a6979b51
renalfarid <enal.farid@gmail.com>
2024-07-01 14:24:41 +0800

    optimize speed performance

--- components/UseAbout.vue
+++ components/UseAbout.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
 <section class="bg-white dark:bg-gray-900">
-  <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+  <div class="bg-white gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
     <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">About Us</h2>
         <p class="mb-4">Berdirinya IT Techno Solutindo guna menjawab tantangan globalisasi serta media industri kreatif dan mengambil peran secara khusus di wilayah timur indonesia dalam bidang Teknologi Informasi dan Komunikasi.</p>
