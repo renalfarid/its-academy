f0d72afafc83cb08a286808e19bc475892276c9a
renalfarid <enal.farid@gmail.com>
2024-07-01 20:08:23 +0800

    remove corporate form

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+const nuxtApp = useNuxtApp();
+const loading = ref(false);
+
 useHead({
   // as a string,
   // where `%s` is replaced with the title
@@ -19,10 +22,22 @@ useHead({
   ]
 
 })
+
+nuxtApp.hook("page:start", () => {
+  loading.value = true;
+});
+nuxtApp.hook("page:finish", () => {
+  loading.value = false;
+});
 </script>
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-    
+ <div class="h-screen">
+    <div
+      v-if="loading"
+      class="fixed left-0 top-0 h-0.5 w-full z-50 bg-green-500"
+    ></div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>   
 </template>
--- components/corporate/RegistrationForm.vue
+++ components/corporate/RegistrationForm.vue
@@ -1,13 +1,7 @@
 <script setup>
-import { onMounted } from 'vue'
-import { createWidget } from '@typeform/embed'
-import '@typeform/embed/build/css/widget.css'
-
-onMounted(() => {
-  createWidget('NEtUtzmA', { container: document.querySelector('#form') })
-})
+//
 </script>
 
 <template>
-  <div id="form" data-tf-widget="NEtUtzmA" class="h-screen w-full"></div>
+  <!-- under construction -->
 </template>
