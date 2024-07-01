b43e4f9076125a42f06b8869f6ddab5d88b27e9b
renalfarid <enal.farid@gmail.com>
2024-07-01 14:48:03 +0800

    seo twitter card

--- app.vue
+++ app.vue
@@ -5,11 +5,17 @@ useHead({
   titleTemplate: '%s',
   meta: [
     { property: 'og:title', content: `Raih Karir Impian bersama ITS Academy` },
+    { property: 'og:site_name', content: `https://its-academy.vercel.app` },
     { property: 'og:description', content: 'ITS Academy hadir untuk membantu Anda mengasah keterampilan IT melalui program-program pelatihan yang komprehensif dan praktis.' },
     { property: 'og:type', content: 'website' },
     { property: 'og:url', content: 'https://its-academy.vercel.app' },
     { property: 'og:locale', content: 'id_ID' },
     { property: 'og:image', content: 'https://its-academy.vercel.app/img/computer-class.png' },
+    { property: 'twitter:title', content: `Raih Karir Impian bersama ITS Academy` },
+    { property: 'twitter:card', content: `Raih Karir Impian bersama ITS Academy` },
+    { property: 'twitter:description', content: 'ITS Academy hadir untuk membantu Anda mengasah keterampilan IT melalui program-program pelatihan yang komprehensif dan praktis.' },
+    { property: 'twitter:image', content: 'https://its-academy.vercel.app/img/computer-class.png' },
+
   ]
 
 })
