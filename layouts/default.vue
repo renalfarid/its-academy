3c2e5799d017e1ae4393103dd56f8f27e143f654
renalfarid <enal.farid@gmail.com>
2024-07-01 13:17:08 +0800

    fix speed insight

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <script setup>
+  import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
 <template>
+    <SpeedInsights />
     <TopMenu />
     <Announcement />
     <div class="mx-auto max-w-screen-xl py-6 px-4 sm:px-6 lg:px-8">
